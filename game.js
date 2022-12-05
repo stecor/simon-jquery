0a47fb7cf1f48be6d2a9ebd8957691e78dcf6905
Stefano Corra <stefano.corra@hotmail.com>
2022-12-05 18:00:30 -0500

    backend

--- game.js
+++ game.js
@@ -1,1 +1,12 @@
+let gamePattern = []
+let buttonColours = ['red', 'blue', 'green', 'yellow']
+let randomChosenColour = ''
+let randomNumber = 0
 
+function nextSequence() {
+  randomNumber = Math.floor(Math.random() * 3)
+  return randomNumber
+}
+
+randomChosenColour = buttonColours[nextSequence()]
+gamePattern.push(randomChosenColour)
